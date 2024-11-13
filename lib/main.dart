4ed700395ec1725dde1c6896bba9962ea5bd67e1
mohamed hisham <mohamedhisham67751@gmail.com>
2024-11-13 03:03:33 +0200

    update code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:chat_app/authentication/views/login_screen.dart';
 import 'package:chat_app/providers/authentication_provider.dart';
+import 'package:chat_app/screens/home_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
